1ff64ab7e17e0e624e431e28bc4d1f6a82afa53f
LucasYamazaki <85938895+LucasYamazaki@users.noreply.github.com>
2021-06-28 11:15:40 -0300

    Criando forms de email

---
+++ core/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.core.mail.message import EmailMessage
+
+class ContatoForm(forms.Form):
+    nome = forms.CharField(label='Nome')
+    email = forms.EmailField(label='Email')
+    assunto = forms.CharField(label='Assunto')
+    mensagem = forms.CharField(label='Mensagem', widget=forms.Textarea())
+
+    def send_mail(self):
+        nome = self.cleaned_data['nome']
+        email = self.cleaned_data['email']
+        assunto = self.cleaned_data['assunto']
+        mensagem = self.cleaned_data['mensagem']
+
+        conteudo = f'Nome: {nome}\nEmail: {email}\nAssunto: {assunto}\nMensagem: {mensagem}'
+
+        mail = EmailMessage(
+            subject= 'Email enviado pelo django2',
+            body=conteudo,
+            from_email='lu.yamazaki@gmail.com',
+            to=['lucasyamazaki@hotmail.com'],
+            headers={'Reply-To' : email}
+        )
+        mail.send()
